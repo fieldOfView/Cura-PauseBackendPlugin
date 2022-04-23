2650a89c4af0a1b66b47243c2e1cbf79515d1296
fieldOfView <aldo@fieldofview.com>
2022-04-23 18:29:06 +0200

    Add support for PyQt6

--- PauseBackend.py
+++ PauseBackend.py
@@ -1,11 +1,6 @@
 # Copyright (c) 2022 Aldo Hoeben / fieldOfView
 # PauseBackendPlugin is released under the terms of the AGPLv3 or higher.
 
-try:
-    from PyQt6.QtCore import QTimer
-except ImportError:
-    from PyQt5.QtCore import QTimer
-
 from UM.Extension import Extension
 from UM.Application import Application
 from UM.PluginRegistry import PluginRegistry
@@ -13,8 +8,14 @@ from UM.Logger import Logger
 
 from UM.Backend.Backend import BackendState
 
-from PyQt5.QtQml import QQmlComponent, QQmlContext
-from PyQt5.QtCore import QUrl, pyqtSlot, QObject
+USE_QT5 = False
+try:
+    from PyQt6.QtQml import QQmlComponent, QQmlContext
+    from PyQt6.QtCore import QUrl, pyqtSlot, QObject, QTimer
+except ImportError:
+    USE_QT5 = True
+    from PyQt5.QtQml import QQmlComponent, QQmlContext
+    from PyQt5.QtCore import QUrl, pyqtSlot, QObject, QTimer
 
 import os.path
 
@@ -36,10 +37,13 @@ class PauseBackend(QObject, Extension):
             # Since this plugin version is only compatible with Cura 3.5 and newer, it is safe to assume API 5
             major_api_version = 5
 
-        if major_api_version <= 5:
-            qml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "PauseBackend3x.qml")
+        if USE_QT5:
+            if major_api_version <= 5:
+                qml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "PauseBackend3x.qml")
+            else:
+                qml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "PauseBackend4x.qml")
         else:
-            qml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "PauseBackend4x.qml")
+            qml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "PauseBackend5x.qml")
 
         self._additional_components = Application.getInstance().createQmlComponent(qml_path, {"manager": self})
         if not self._additional_components:
---
+++ PauseBackend5x.qml
@@ -0,0 +1,58 @@
+// Copyright (c) 2022 Aldo Hoeben / fieldOfView
+// PauseBackendPlugin is released under the terms of the AGPLv3 or higher.
+
+import UM 1.2 as UM
+import Cura 1.0 as Cura
+
+import QtQuick 2.2
+import QtQuick.Controls 2.3
+
+Item
+{
+    id: base
+
+    Cura.SecondaryButton
+    {
+        id: pauseResumeButton
+        objectName: "pauseResumeButton"
+
+        function boolCheck(value) //Hack to ensure a good match between python and qml.
+        {
+            if(value == "True")
+            {
+                return true;
+            }
+            else if(value == "False" || value == undefined)
+            {
+                return false;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        function togglePaused()
+        {
+            paused = !paused;
+            UM.Preferences.setValue("general/auto_slice", !paused);
+        }
+
+        property bool paused: !boolCheck(UM.Preferences.getValue("general/auto_slice"))
+
+        height: UM.Theme.getSize("action_button").height
+        iconSource: Qt.resolvedUrl(paused ? "play.svg" : "pause.svg")
+
+        tooltip: paused ? catalog.i18nc("@info:tooltip", "Resume automatic slicing") : catalog.i18nc("@info:tooltip", "Pause automatic slicing")
+        toolTipContentAlignment: UM.Enums.ContentAlignment.AlignRight
+
+        onClicked: togglePaused()
+        Action
+        {
+            shortcut: "Ctrl+Shift+P"
+            onTriggered: pauseResumeButton.togglePaused()
+        }
+    }
+
+    UM.I18nCatalog{id: catalog; name:"cura"}
+}
